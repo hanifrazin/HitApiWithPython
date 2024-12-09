227158ab06dad52aa348c973c2364e7c14217043
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2024-12-09 19:59:45 +0700

    Ikutin tutorial automation api pytest

--- data.py
+++ data.py
@@ -1,2 +1,27 @@
 class Token:
-    value = {"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczMzIyMTA4MCwianRpIjoiYTNhYmU2MTUtYTBlZC00ODliLWJjMWYtNmFjODhkYzZjMjg0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFkbWluIiwibmJmIjoxNzMzMjIxMDgwLCJleHAiOjE3MzMyMjQ2ODB9.ojJ5YaNQ8dTf82yoFQ53WIvykX67bfffaaUv1GzpDLk"}
+    value = {"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczMzczMzI4OSwianRpIjoiMTQxN2QwNGMtMjhjZC00ZTQyLWI4OTgtYzViMjZhNmVhMmRhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFkbWluIiwibmJmIjoxNzMzNzMzMjg5LCJleHAiOjE3MzM3MzY4ODl9.0RhdBkj1ROiQtNl0qo5Lv-leBVkeUVN_47Wl-ckIYlo"}
+
+class Schema:
+    schema_get_item = {
+        "type": "object",
+        "properties": {
+            "description": {
+                "type": "string"
+            },
+            "id": {
+                "type": "integer"
+            },
+            "name": {
+                "type": "string"
+            },
+            "quantity": {
+                "type": "integer"
+            }
+        },
+        "required": [
+            "description",
+            "id",
+            "name",
+            "quantity"
+        ]
+    }
--- test_hitapi.py
+++ test_hitapi.py
@@ -1,19 +1,58 @@
-from pprint import pprint
-
+import json
 import requests
-from data import Token
+from pprint import pprint
+from data import Schema
+from jsonschema import validate, ValidationError
 
 base_url = 'https://testapi.kelasotomesyen.com'
 
-token = Token.value
-
 #get method
-resp = requests.get(f"{base_url}/items", headers=token)
-pprint(resp.json())
+# resp = requests.get(f"{base_url}/items/1", headers=token)
+# print(resp.status_code)
+# print(resp.text)
+# json_response = json.loads(resp.text)
+# pprint(json_response)
 
+# schema get item
+
+def get_token():
+    body = {
+        "username": "admin",
+        "password": "uHuY12#$"
+    }
+    resp = requests.post(f'{base_url}/login', json=body)
+    json_resp = json.loads(resp.text)
+    return {"Authorization": f"Bearer {json_resp.get('token')}"}
 
 def test_get_items():
-    resp = requests.get(f"{base_url}/items", headers=token)
+    resp = requests.get(f"{base_url}/items/1", headers=get_token())
+
     status_code = resp.status_code
+    json_response = json.loads(resp.text)
+    id = json_response.get('id')
+    name = json_response.get('name')
 
     assert status_code == 200
+    assert id == 1
+    assert name == 'Dodol'
+
+    try:
+        validate(resp.json(), schema=Schema.schema_get_item)
+    except ValidationError as e:
+        assert False, f'JSON Schema error: {e}'
+
+def test_post_items():
+    body = {
+        "name": "Bakso Tahu",
+        "description": "Jajanan Traditional",
+        "quantity": 10
+    }
+    resp = requests.post(f"{base_url}/items", json=body, headers=get_token())
+
+    status_code = resp.status_code
+    json_response = json.loads(resp.text)
+    pprint(json_response)
+
+
+test_post_items()
+
